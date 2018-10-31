ce63844e1e8681434ea2d70c3fe83215aea4c554
alvaro9650 <alvaromorenonavarro1999@hotmail.es>
2018-10-31 12:46:23 +0100

    Añadir firebase analytics a la app

--- app/src/main/java/com/example/alumno/firebasechat/Contactos.kt
+++ app/src/main/java/com/example/alumno/firebasechat/Contactos.kt
@@ -2,14 +2,15 @@ package com.example.alumno.firebasechat
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_contactos.*
 
 class Contactos : AppCompatActivity() {
-
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contactos)
-
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
     }
